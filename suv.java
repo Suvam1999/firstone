32868dadd22cbc5f403c31878c094018f88077bc
Suvam1999 <48570927+Suvam1999@users.noreply.github.com>
2019-05-20 19:25:53 +0530

    printed thrice

--- suv.java
+++ suv.java
@@ -4,5 +4,9 @@ public class me
 public static void main(String args[] )throws ioException
 {
 system.out.println("i am suvam ");
+  
+system.out.println("i am suvam ");
+  
+system.out.println("i am suvam ");
 }
 }
